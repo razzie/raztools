69a63a9fb43a458df0871e1add26c115d665153c
Razzie <hello@razzie.me>
2018-09-10 20:35:24 +0200

    Added examples project

---
+++ examples/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Copyright (C) Gábor "Razzie" Görzsöny
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+*/
+
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace examples
+{
+    class Program
+    {
+        static private Type[] Examples { get; } = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(IExample))).ToArray();
+
+        static int Main(string[] args)
+        {
+            if (Examples.Length > 0)
+            {
+                for (int i = 0; i < Examples.Length; ++i)
+                {
+                    Console.WriteLine("#{0} - {1}", i + 1, Examples[i].Name);
+                }
+                
+                var selection = int.Parse(Console.ReadLine());
+                var example = Activator.CreateInstance(Examples[selection]) as IExample;
+                return example.Run();
+            }
+
+            return 0;
+        }
+    }
+}
